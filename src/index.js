7f3e73309b4bd7d362d905af13a1b3bb0fdd3d0e
Edgar Cabrera <edgarcabrera33@gmail.com>
2025-01-22 13:58:27 -0800

    Add sitemap.xml to public directory and update related files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('No se encontró el elemento con ID "root"');
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
