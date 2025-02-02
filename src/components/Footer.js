20bfb1d9e16550299fed84fed59c30457ac93bb6
Edgar Cabrera <edgarcabrera33@gmail.com>
2025-02-02 15:11:10 -0800

    new update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "../styles/footer.css";
 const Footer = () => {
   return (
     <footer class="footer">
-      <p>&copy; 2024 EZ LIVING - Todos los derechos reservados</p>
+      <p>&copy; 2025 EZ LIVING - Todos los derechos reservados</p>
       <p>Edgar Cabrera</p>
     </footer>
   );
--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error('No se encontró el elemento con ID "root"');
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
